e145b238016572b8e19516f3107a7ecdd52830f1
tameraktasTU <tamer.aktas@web.de>
2023-02-26 00:18:10 +0100

    initial commit

---
+++ js-sdk-0.11.1/src/Client.ts
@@ -0,0 +1,347 @@
+import ClientResponseError from '@/ClientResponseError';
+import BaseAuthStore       from '@/stores/BaseAuthStore';
+import LocalAuthStore      from '@/stores/LocalAuthStore';
+import SettingsService     from '@/services/SettingsService';
+import AdminService        from '@/services/AdminService';
+import RecordService       from '@/services/RecordService';
+import CollectionService   from '@/services/CollectionService';
+import LogService          from '@/services/LogService';
+import RealtimeService     from '@/services/RealtimeService';
+import HealthService       from '@/services/HealthService';
+import Record              from '@/models/Record';
+import { FileQueryParams } from '@/services/utils/QueryParams';
+
+/**
+ * PocketBase JS Client.
+ */
+export default class Client {
+    /**
+     * The base PocketBase backend url address (eg. 'http://127.0.0.1.8090').
+     */
+    baseUrl: string;
+
+    /**
+     * Hook that get triggered right before sending the fetch request,
+     * allowing you to inspect/modify the request config.
+     *
+     * Returns the new modified config that will be used to send the request.
+     *
+     * For list of the possible options check https://developer.mozilla.org/en-US/docs/Web/API/fetch#options
+     *
+     * Example:
+     * ```js
+     * client.beforeSend = function (url, reqConfig) {
+     *     reqConfig.headers = Object.assign({}, reqConfig.headers, {
+     *         'X-Custom-Header': 'example',
+     *     });
+     *
+     *     return reqConfig;
+     * };
+     * ```
+     */
+    beforeSend?: (url: string, reqConfig: { [key: string]: any }) => { [key: string]: any };
+
+    /**
+     * Hook that get triggered after successfully sending the fetch request,
+     * allowing you to inspect/modify the response object and its parsed data.
+     *
+     * Returns the new Promise resolved `data` that will be returned to the client.
+     *
+     * Example:
+     * ```js
+     * client.afterSend = function (response, data) {
+     *     if (response.status != 200) {
+     *         throw new ClientResponseError({
+     *             url:      response.url,
+     *             status:   response.status,
+     *             data:     data,
+     *         });
+     *     }
+     *
+     *     return data;
+     * };
+     * ```
+     */
+    afterSend?: (response: Response, data: any) => any;
+
+    /**
+     * Optional language code (default to `en-US`) that will be sent
+     * with the requests to the server as `Accept-Language` header.
+     */
+    lang: string;
+
+    /**
+     * A replaceable instance of the local auth store service.
+     */
+    authStore: BaseAuthStore;
+
+    /**
+     * An instance of the service that handles the **Settings APIs**.
+     */
+    readonly settings: SettingsService;
+
+    /**
+     * An instance of the service that handles the **Admin APIs**.
+     */
+    readonly admins: AdminService;
+
+    /**
+     * An instance of the service that handles the **Collection APIs**.
+     */
+    readonly collections: CollectionService;
+
+    /**
+     * An instance of the service that handles the **Log APIs**.
+     */
+    readonly logs: LogService;
+
+    /**
+     * An instance of the service that handles the **Realtime APIs**.
+     */
+    readonly realtime: RealtimeService;
+
+    /**
+     * An instance of the service that handles the **Health APIs**.
+     */
+    readonly health: HealthService;
+
+    private cancelControllers: { [key: string]: AbortController } = {};
+    private recordServices: { [key: string]: RecordService } = {};
+    private enableAutoCancellation: boolean = true;
+
+    constructor(
+        baseUrl = '/',
+        authStore?: BaseAuthStore | null,
+        lang = 'en-US',
+    ) {
+        this.baseUrl   = baseUrl;
+        this.lang      = lang;
+        this.authStore = authStore || new LocalAuthStore();
+
+        // services
+        this.admins      = new AdminService(this);
+        this.collections = new CollectionService(this);
+        this.logs        = new LogService(this);
+        this.settings    = new SettingsService(this);
+        this.realtime    = new RealtimeService(this);
+        this.health      = new HealthService(this);
+    }
+
+    /**
+     * Returns the RecordService associated to the specified collection.
+     *
+     * @param  {string} idOrName
+     * @return {RecordService}
+     */
+    collection(idOrName: string): RecordService {
+        if (!this.recordServices[idOrName]) {
+            this.recordServices[idOrName] = new RecordService(this, idOrName);
+        }
+
+        return this.recordServices[idOrName];
+    }
+
+    /**
+     * Globally enable or disable auto cancellation for pending duplicated requests.
+     */
+    autoCancellation(enable: boolean): Client {
+        this.enableAutoCancellation = !!enable;
+
+        return this;
+    }
+
+    /**
+     * Cancels single request by its cancellation key.
+     */
+    cancelRequest(cancelKey: string): Client {
+        if (this.cancelControllers[cancelKey]) {
+            this.cancelControllers[cancelKey].abort();
+            delete this.cancelControllers[cancelKey];
+        }
+
+        return this;
+    }
+
+    /**
+     * Cancels all pending requests.
+     */
+    cancelAllRequests(): Client {
+        for (let k in this.cancelControllers) {
+            this.cancelControllers[k].abort();
+        }
+
+        this.cancelControllers = {};
+
+        return this;
+    }
+
+    /**
+     * Sends an api http request.
+     */
+    async send(path: string, reqConfig: { [key: string]: any }): Promise<any> {
+        let config = Object.assign({ method: 'GET' } as { [key: string]: any }, reqConfig);
+
+        // serialize the body if needed and set the correct content type
+        // note1: for FormData body the Content-Type header should be skipped
+        // note2: we are checking the constructor name because FormData is not available natively in node
+        if (config.body && config.body.constructor.name !== 'FormData') {
+            if (typeof config.body !== 'string') {
+                config.body = JSON.stringify(config.body);
+            }
+
+            // add the json header (if not already)
+            if (typeof config?.headers?.['Content-Type'] === 'undefined') {
+                config.headers = Object.assign({}, config.headers, {
+                    'Content-Type': 'application/json',
+                });
+            }
+        }
+
+        // add Accept-Language header (if not already)
+        if (typeof config?.headers?.['Accept-Language'] === 'undefined') {
+            config.headers = Object.assign({}, config.headers, {
+                'Accept-Language': this.lang,
+            });
+        }
+
+        // check if Authorization header can be added
+        if (
+            // has stored token
+            this.authStore?.token &&
+            // auth header is not explicitly set
+            (typeof config?.headers?.Authorization === 'undefined')
+        ) {
+            config.headers = Object.assign({}, config.headers, {
+                'Authorization': this.authStore.token,
+            });
+        }
+
+        // handle auto cancelation for duplicated pending request
+        if (this.enableAutoCancellation && config.params?.$autoCancel !== false) {
+            const cancelKey = config.params?.$cancelKey || ((config.method || 'GET') + path);
+
+            // cancel previous pending requests
+            this.cancelRequest(cancelKey);
+
+            const controller = new AbortController();
+            this.cancelControllers[cancelKey] = controller;
+            config.signal = controller.signal;
+        }
+        // remove the special cancellation params from the other valid query params
+        delete config.params?.$autoCancel;
+        delete config.params?.$cancelKey;
+
+        // build url + path
+        let url = this.buildUrl(path);
+
+        // serialize the query parameters
+        if (typeof config.params !== 'undefined') {
+            const query = this.serializeQueryParams(config.params)
+            if (query) {
+                url += (url.includes('?') ? '&' : '?') + query;
+            }
+            delete config.params;
+        }
+
+        if (this.beforeSend) {
+            config = Object.assign({}, this.beforeSend(url, config));
+        }
+
+        // send the request
+        return fetch(url, config)
+            .then(async (response) => {
+                let data : any = {};
+
+                try {
+                    data = await response.json();
+                } catch (_) {
+                    // all api responses are expected to return json
+                    // with the exception of the realtime event and 204
+                }
+
+                if (this.afterSend) {
+                    data = this.afterSend(response, data);
+                }
+
+                if (response.status >= 400) {
+                    throw new ClientResponseError({
+                        url:      response.url,
+                        status:   response.status,
+                        data:     data,
+                    });
+                }
+
+                return data;
+            }).catch((err) => {
+                // wrap to normalize all errors
+                throw new ClientResponseError(err);
+            });
+    }
+
+    /**
+     * Builds and returns an absolute record file url for the provided filename.
+     */
+    getFileUrl(
+        record: Pick<Record, "id" | "collectionId" | "collectionName">,
+        filename: string,
+        queryParams: FileQueryParams = {}
+    ): string {
+        const parts = [];
+        parts.push("api")
+        parts.push("files")
+        parts.push(encodeURIComponent(record.collectionId || record.collectionName))
+        parts.push(encodeURIComponent(record.id))
+        parts.push(encodeURIComponent(filename))
+
+        let result = this.buildUrl(parts.join('/'));
+
+        if (Object.keys(queryParams).length) {
+            const params = new URLSearchParams(queryParams);
+            result += (result.includes("?") ? "&" : "?") + params;
+        }
+
+        return result
+    }
+
+    /**
+     * Builds a full client url by safely concatenating the provided path.
+     */
+    buildUrl(path: string): string {
+        let url = this.baseUrl + (this.baseUrl.endsWith('/') ? '' : '/');
+        if (path) {
+            url += (path.startsWith('/') ? path.substring(1) : path);
+        }
+        return url;
+    }
+
+    /**
+     * Serializes the provided query parameters into a query string.
+     */
+    private serializeQueryParams(params: {[key: string]: any}): string {
+        const result: Array<string> = [];
+        for (const key in params) {
+            if (params[key] === null) {
+                // skip null query params
+                continue;
+            }
+
+            const value = params[key];
+            const encodedKey = encodeURIComponent(key);
+
+            if (Array.isArray(value)) {
+                // "repeat" array params
+                for (const v of value) {
+                    result.push(encodedKey + "=" + encodeURIComponent(v));
+                }
+            } else if (value instanceof Date) {
+                result.push(encodedKey + "=" + encodeURIComponent(value.toISOString()));
+            } else if (typeof value !== null && typeof value === 'object') {
+                result.push(encodedKey + "=" + encodeURIComponent(JSON.stringify(value)));
+            } else {
+                result.push(encodedKey + "=" + encodeURIComponent(value));
+            }
+        }
+
+        return result.join('&');
+    }
+}
---
+++ js/scripts.js
@@ -0,0 +1,109 @@
+const url = "https://whitestone.pockethost.io"
+const client = new PocketBase(url);
+var userID = localStorage.getItem("userID");
+var currentKey;
+var ownKeys;
+
+async function checkLoginStatus() {
+    if (!client.authStore.isValid) {
+        location.href = "index.html";
+    }
+}
+
+async function checkAlreadyLoggedIn() {
+    if (client.authStore.isValid) {
+        location.href = "overview.html";
+    }
+}
+
+async function validateLogin() {
+    var nutzername = document.getElementById("nutzername").value;
+    var passwort = document.getElementById("passwort").value;
+    if (!nutzername || !passwort) {
+        console.log("test");
+    } else {
+        const authData = await client.collection('users').authWithPassword(nutzername, passwort);
+        localStorage.setItem("userID", client.authStore.model.id);
+        location.href = "overview.html";
+    }
+}
+
+async function getMitarbeiterName() {
+    let data = await client.collection("users").getOne(userID);
+    document.getElementById("welcomeMessage").innerHTML = "Hey " + data.firstName;
+}
+
+async function getKeyInformation() {
+    let searchValue = document.getElementById("searchValue").value;
+    let searchQuery = "keyNumber=" + searchValue;
+    let data = await client.collection("keys").getFirstListItem(searchQuery);
+    currentKey = data;
+    displaySearchedKey();
+}
+
+async function displaySearchedKey() {
+    checkOwnerKey();
+    document.getElementById("keyID").innerHTML = "Schlüssel-Nr: " + currentKey.keyNumber;
+    document.getElementById("hersteller").innerHTML = "Hersteller: " + currentKey.autohersteller;
+    document.getElementById("modell").innerHTML = "Modell: " + currentKey.automodell;
+    document.getElementById("FIN").innerHTML = "FIN: " + currentKey.fin;
+}
+
+async function checkOwnerKey() {
+    for (let i = 0; i < ownKeys.length; i++) {
+        if (ownKeys[i].keyNumber == currentKey.keyNumber) {
+            document.getElementById("besitzerKey").innerHTML = "Du bist Besitzer dieses Schlüssels!";
+            break;
+        } else {
+            document.getElementById("besitzerKey").innerHTML = "Der Schlüssel gehört nicht dir!"
+        }
+    }
+}
+
+function displayOwnKeys() {
+    const parentElement = document.getElementById("myKeys");
+    parentElement.innerHTML = "";
+    for (let i = 0; i < ownKeys.length; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("box", "has-background-grey-lighter");
+        const newP1 = document.createElement("p");
+        const newP2 = document.createElement("p");
+        const newP3 = document.createElement("p");
+        const newP4 = document.createElement("p");
+        newP1.textContent = "Schlüssel-Nr: " + ownKeys[i].keyNumber;
+        newP2.textContent = "Hersteller: " + ownKeys[i].autohersteller;
+        newP3.textContent = "Modell: " + ownKeys[i].automodell;
+        newP4.textContent = "FIN: " + ownKeys[i].fin;
+        newDiv.appendChild(newP1);
+        newDiv.appendChild(newP2);
+        newDiv.appendChild(newP3);
+        newDiv.appendChild(newP4);
+        parentElement.appendChild(newDiv);
+    }
+}
+
+async function getOwnerKeys() {
+    let searchQuery = "keyOwner = '" + userID + "'";
+    let data = await client.collection("keys").getFullList(200, {
+        filter: searchQuery,
+    });
+    ownKeys = data;
+    displayOwnKeys()
+}
+
+async function transferKey() {
+
+}
+
+function switchView() {
+    if (document.getElementById("keysView").classList.contains("is-hidden")) {
+        document.getElementById("infoView").classList.add("is-hidden");
+        document.getElementById("keysView").classList.remove("is-hidden");
+    }
+    else if (document.getElementById("infoView").classList.contains("is-hidden")) {
+        document.getElementById("keysView").classList.add("is-hidden");
+        document.getElementById("infoView").classList.remove("is-hidden");
+    }
+}
+
+
