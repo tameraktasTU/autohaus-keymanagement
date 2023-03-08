18aba7f3d062a8249215c9c818584cb88331d327
tameraktasTU <tamer.aktas@web.de>
2023-03-08 01:22:15 +0100

    update to incorporate new fahrten view added new bottom nav option

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ const client = new PocketBase(url);
 var transferButton = document.getElementById("transferButton");
 var currentKey;
 var ownKeys;
+var ownFahrten;
 
 async function checkLoginStatus() {
     if (!client.authStore.isValid) {
@@ -119,15 +120,78 @@ function displayOwnKeys() {
     }
 }
 
-function switchView() {
-    if (document.getElementById("keysView").classList.contains("is-hidden")) {
-        document.getElementById("infoView").classList.add("is-hidden");
-        document.getElementById("keysView").classList.remove("is-hidden");
-    }
-    else if (document.getElementById("infoView").classList.contains("is-hidden")) {
-        document.getElementById("keysView").classList.add("is-hidden");
-        document.getElementById("infoView").classList.remove("is-hidden");
+async function loadFahrten() {
+    let searchQuery = "fahrer = '" + client.authStore.model.id + "'";
+    let data = await client.collection('fahrten').getFullList(200, {
+        searchQuery
+    });
+    ownFahrten = data;
+    displayOwnFahrten();
+}
+
+function displayOwnFahrten() {
+    const parentElement = document.getElementById("myFahrten");
+    parentElement.innerHTML = "";
+    for (let i = 0; i < ownFahrten.length; i++) {
+
+        const datum = new Date(ownFahrten[i].datum);
+
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("box", "has-background-grey-lighter");
+
+        const newP1 = document.createElement("p");
+        newP1.classList.add("is-size-6")
+        const newP2 = document.createElement("p");
+        newP2.classList.add("is-size-5", "has-text-weight-bold");
+        const newP3 = document.createElement("p");
+        newP3.classList.add("is-size-5");
+
+        const newP4 = document.createElement("p");
+        newP1.textContent = datum.toLocaleDateString("de-DE");
+        newP2.textContent = ownFahrten[i].abfahrtsOrt + " > " + ownFahrten[i].ankunftsOrt;
+        newP3.textContent = ownFahrten[i].kennzeichen + " | " + ownFahrten[i].distanz + "km";
+        //newP4.textContent = "FIN: " + ownFahrten[i].fin;
+        newDiv.appendChild(newP1);
+        newDiv.appendChild(newP2);
+        newDiv.appendChild(newP3);
+        //newDiv.appendChild(newP4);
+        parentElement.appendChild(newDiv);
     }
 }
 
+function displayKeysPage() {
+    if (document.getElementById("keysView").classList.contains("is-hidden")) {
+        checkLoginStatus();
+        document.getElementById("fahrtenView").classList.remove("is-active")
+        document.getElementById("infoView").classList.remove("is-active");
+        document.getElementById("fahrtenView").classList.add("is-hidden")
+        document.getElementById("infoView").classList.add("is-hidden");
+        document.getElementById("keysView").classList.remove("is-hidden");
+        document.getElementById("keysView").classList.add("is-active");
+    }
+}
 
+function displayInfoPage() {
+    if (document.getElementById("infoView").classList.contains("is-hidden")) {
+        checkLoginStatus();
+        document.getElementById("fahrtenView").classList.remove("is-active")
+        document.getElementById("keysView").classList.remove("is-active");
+        document.getElementById("fahrtenView").classList.add("is-hidden")
+        document.getElementById("keysView").classList.add("is-hidden");
+        document.getElementById("infoView").classList.remove("is-hidden");
+        document.getElementById("infoView").classList.add("is-active");
+    }
+}
+
+function displayFahrtenPage() {
+    if (document.getElementById("fahrtenView").classList.contains("is-hidden")) {
+        checkLoginStatus();
+        loadFahrten();
+        document.getElementById("infoView").classList.remove("is-active")
+        document.getElementById("keysView").classList.remove("is-active");
+        document.getElementById("infoView").classList.add("is-hidden")
+        document.getElementById("keysView").classList.add("is-hidden");
+        document.getElementById("fahrtenView").classList.remove("is-hidden");
+        document.getElementById("fahrtenView").classList.add("is-active");
+    }
+}
